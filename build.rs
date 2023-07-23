4cf6560e6462b1b549dc33b1a94b2ad67f2ee8ae
Alex Pearson <alex@alexpear.com>
2023-07-23 13:49:43 -0400

    🔨 Rename btrfs whitelist to allowlist

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-struct BindgenWhitelist {
+struct BindgenAllowlist {
     types: Option<Vec<String>>,
     functions: Option<Vec<String>>,
     vars: Option<Vec<String>>,
@@ -12,29 +12,29 @@ struct BindgenWhitelist {
 
 fn build() {
     println!("cargo:rerun-if-changed=libbtrfsutil/btrfsutil.h");
-    println!("cargo:rerun-if-changed=bindgen_whitelist.toml");
+    println!("cargo:rerun-if-changed=bindgen_allowlist.toml");
 
-    let bindgen_whitelist_string: String = String::from_utf8(
-        std::fs::read("bindgen_whitelist.toml").expect("Failed to open bindgen_whitelist.toml"),
+    let bindgen_allowlist_string: String = String::from_utf8(
+        std::fs::read("bindgen_allowlist.toml").expect("Failed to open bindgen_allowlist.toml"),
     )
-    .expect("Failed to convert the whitelist file to UTF-8 string");
-    let bindgen_whitelist: BindgenWhitelist = toml::from_str(bindgen_whitelist_string.as_str())
-        .expect("Failed to deserialize bindgen whitelist");
+    .expect("Failed to convert the allowlist file to UTF-8 string");
+    let bindgen_allowlist: BindgenAllowlist = toml::from_str(bindgen_allowlist_string.as_str())
+        .expect("Failed to deserialize bindgen allowlist");
 
     let mut bindings_builder: bindgen::Builder =
         bindgen::Builder::default().header("libbtrfsutil/btrfsutil.h");
 
-    if let Some(val) = bindgen_whitelist.types {
+    if let Some(val) = bindgen_allowlist.types {
         for type_name in val {
             bindings_builder = bindings_builder.allowlist_type(type_name);
         }
     }
-    if let Some(val) = bindgen_whitelist.functions {
+    if let Some(val) = bindgen_allowlist.functions {
         for function_name in val {
             bindings_builder = bindings_builder.allowlist_function(function_name);
         }
     }
-    if let Some(val) = bindgen_whitelist.vars {
+    if let Some(val) = bindgen_allowlist.vars {
         for var_name in val {
             bindings_builder = bindings_builder.allowlist_var(var_name);
         }
